70e6c989e4a0cfe2898a3edf300a5c2b6fcc58bc
Ayush-2301 <ayushchauhan233@gmail.com>
2024-05-22 19:38:36 +0530

    minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+// fix
 import { Toaster } from "@/components/ui/toaster";
 import { createClient } from "@/utils/supabase/server";
 const inter = Inter({ subsets: ["latin"] });
